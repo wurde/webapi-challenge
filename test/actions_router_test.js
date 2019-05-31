95a0f5d008e77d552c19925183eeb33c6e266a1b
Andy Bettisworth <andy@accreu.com>
2019-05-31 12:30:26 -0500

    Add placeholder test files

---
+++ test/actions_router_test.js
@@ -0,0 +1,15 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const test = require('ava')
+
+/**
+ * Assertions
+ */
+
+test("Add tests for actions_router!", t => {
+  t.pass()
+})
---
+++ test/root_router_test.js
@@ -0,0 +1,15 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const test = require('ava')
+
+/**
+ * Assertions
+ */
+
+test("Add tests for root_router!", t => {
+  t.pass()
+})
